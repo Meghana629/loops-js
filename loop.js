7fe1e10e79f8fef3504031ef16b7a7ac0ff233d4
Meghana629 <meghanakatkam01@gmail.com>
2024-10-24 10:05:26 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,69 @@
+// for loop examples
+// example1
+
+for(var a=1;a<10;a++){
+    console.log(a)
+}
+
+
+// example2
+for(var a=11;a>6;a--){
+    console.log(a)
+}
+
+
+// example3
+for(var a=0;a<=5;a++){
+
+}
+console.log(a)
+
+
+
+// while loop examples
+// example1
+
+var a=1;
+while(a<=7){
+    console.log(a)
+    a++
+}
+
+// example2
+var a=10;
+while(a>=6){
+    console.log(a)
+    a--
+}
+
+// example3
+var a=1;
+while(a<4){
+    a++
+}
+console.log(a)
+
+// do-while loop
+// example1
+var a=1;
+do{
+    a++
+    console.log(a)
+}while(a<=5)
+
+// example2
+
+var a=10;
+do{
+    console.log(a)
+    a--
+}while(a>=7)
+
+//example3
+
+var a=10;
+do{
+    a--
+    console.log(a)
+}while(a>=7)
+
